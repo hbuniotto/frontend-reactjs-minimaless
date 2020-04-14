27574f9d43a8d65c6fcd3040337bf0ec99d72533
Humberto Buniotto <humbertobuniotto@Humbertos-MacBook-Pro.local>
2020-04-13 21:02:29 -0400

    init

---
+++ src/views/Account/Account.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Grid } from '@material-ui/core';
+
+import MyListings from './components/MyListings';
+import { AccountProfile, AccountDetails } from './components';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  }
+}));
+
+const Account = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={4}
+      >
+        <Grid
+          item
+          lg={6}
+          md={6}
+          xl={6}
+          xs={12}
+        >
+          <AccountProfile />
+        </Grid>
+        <Grid
+          item
+          lg={6}
+          md={6}
+          xl={6}
+          xs={12}
+        >
+          <AccountDetails />
+        </Grid>
+        <Grid
+          item
+          lg={6}
+          md={6}
+          xl={6}
+          xs={12}
+        >
+          <MyListings />
+      </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Account;
---
+++ src/views/NotFound/NotFound.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Grid, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  },
+  content: {
+    paddingTop: 150,
+    textAlign: 'center'
+  },
+  image: {
+    marginTop: 50,
+    display: 'inline-block',
+    maxWidth: '100%',
+    width: 560
+  }
+}));
+
+const NotFound = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        justify="center"
+        spacing={4}
+      >
+        <Grid
+          item
+          lg={6}
+          xs={12}
+        >
+          <div className={classes.content}>
+            <Typography variant="h1">
+              404: Oh no!...we can't find your page 😨
+            </Typography>
+            <Typography variant="subtitle2">
+              You either tried some shady route or you came here by mistake.
+              Whatever the case, go back and try a different button 
+            </Typography>
+            <img
+              alt="Under development"
+              className={classes.image}
+              src="/images/undraw_page_not_found_su7k.svg"
+            />
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default NotFound;
